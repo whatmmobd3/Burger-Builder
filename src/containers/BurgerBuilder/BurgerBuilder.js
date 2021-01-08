4bb27efa2a441eb29fa279c06d9fb67454812776
Your Name <you@example.com>
2021-01-08 15:54:46 +0700

    update

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Aux from "../../hoc/Aux1";
 
+import Burger from '../../components/Burger/Burger'
+
 class App extends Component {
   render() {
     return (
       <Aux>
-        <div>Burger</div>
+        <Burger />
         <div>Build Controls</div>
       </Aux>
     );
