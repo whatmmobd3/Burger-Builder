d8207766a0d38b9ccf7cb16a54c3bea3d9d1773e
Your Name <you@example.com>
2021-01-15 10:57:58 +0700

    Toolbar

---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import classes from './Toolbar.css'
+
+const App = props => (
+    <header className={classes.Toolbar}>
+        <div>MENU</div>
+        <div>LOGO</div>
+        <nav>
+            ...
+        </nav>
+    </header>
+)
+
+export default App
